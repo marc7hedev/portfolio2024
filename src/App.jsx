2c922af6b42cbb774e5d65352443c37bbcfaa91f
marc7hedev <driberstagg@gmail.com>
2024-03-07 17:07:30 -0600

    Prueba de efectos y animaciones con framermotion

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ const App = () => {
     <section>Contact</section> */}
 
     <Test/>
+    <Test/>
 
   </div>;
 };
--- src/Test.jsx
+++ src/Test.jsx
@@ -1,14 +1,32 @@
 import {motion} from "framer-motion";
+import { useState } from "react";
 
 const Test = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const variants={
+        visible:{
+            opacity:1, 
+            x:100, 
+            transition:{staggerChildren:1}
+        },
+        hidden:{opacity:0},
+    };
+
+    const items = [
+        "item2", "item2", "item3", "item4"
+    ]
+
     return (
         <div className="course">
-            <motion.div className="box" 
-            initial={{opacity:0.5, scale:0.5}} 
-            transition={{duration:2}}
-            whileTap={{opacity:1, scale:2}}
-            drag
-            ></motion.div>
+            
+            <motion.ul initial="hidden" animate="visible" variants={variants}>
+                {items.map((item)=>(
+                    <motion.li key={item} variants={variants}>{item}</motion.li>
+                ))}
+            </motion.ul>
+
         </div>
     )
 }
