7751f2b755a215b14dff0c2bb34a76205fbf6b7a
marc7hedev <driberstagg@gmail.com>
2024-03-07 17:27:21 -0600

    Animando titulo de la página

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return <div>
 
-    {/* <section>
+    <section>
       <Navbar/>
     </section>
     <section>Parallax</section>
@@ -14,10 +14,10 @@ const App = () => {
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section> */}
+    <section>Contact</section>
 
-    <Test/>
-    <Test/>
+    {/* <Test/>
+    <Test/> */}
 
   </div>;
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 import "./navbar.scss"
+import {motion} from "framer-motion";
 
 const Navbar = () => {
     return (
         <div className="navbar">
             {/* Sidebar */}
             <div className="wrapper">
-                <span>Marco Rangel</span>
+                <motion.span 
+                initial={{opacity:0, scale:0.5}}
+                animate={{opacity:1, scale:1}}
+                transition={{duration:0.5}}
+
+                >Marco Rangel</motion.span>
                 <div className="social">
                     <a href="#"><img src="/facebook.png" alt=""/></a>
                     <a href="#"><img src="/instagram.png" alt=""/></a>
