ab1f470da3cb9f95f5788a34dcb930d9e2e5b380
marc7hedev <driberstagg@gmail.com>
2024-03-15 16:44:32 -0600

    Añadida sección de Hero

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,53 @@
+import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+    initial:{
+        x: -500,
+        opacity: 0,
+    },
+    animate:{
+        x:0,
+        opacity: 1,
+        transition:{
+            duration:1,
+            staggerChildren: 0.1,
+        }
+    },
+    scrollButton:{
+        opacity: 0,
+        y:10,
+        transition:{
+            duration: 2,
+            repeat: Infinity,
+        }
+    }
+};
+
+const Hero = () => {
+    return (
+
+        <div className="hero">
+            <div className="wrapper">
+                <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+                    <motion.h2 variants={textVariants}>Marco Rangel</motion.h2>
+                    <motion.h1 variants={textVariants}>Desarrollador Web</motion.h1>
+                    <motion.div variants={textVariants} className="buttons">
+                        <motion.button variants={textVariants}>Mis últimos proyectos</motion.button>
+                        <motion.button variants={textVariants}>Contáctame</motion.button>
+                    </motion.div>
+                    <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt=""/>
+                </motion.div>
+            </div>
+            <div className="slidingTextContainer">
+                Desarrollador Creador Autodidacta
+            </div>
+            <div className="imageContainer">
+                <img src= "/hero.png" alt=""/>
+            </div>
+        </div>
+
+    )
+}
+
+export default Hero
