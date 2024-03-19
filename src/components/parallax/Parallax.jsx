e3df4bb2b94e40f79c58eaf9721623bf6b7c36c7
marc7hedev <driberstagg@gmail.com>
2024-03-19 12:17:57 -0600

    Implementados Hooks useScroll y useTransform

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,8 +1,23 @@
+import { useRef } from "react";
 import "./parallax.scss"
+import { motion, useScroll, useTransform } from "framer-motion";
+
 
 const Parallax = ({type}) => {
+
+    const ref = useRef();
+
+    const {scrollYProgress} = useScroll({
+        target: ref,
+        offset: ["start start", "end start"]
+    })
+    
+    const yText = useTransform(scrollYProgress,[0,1],["0%","500%"])
+    const yBg = useTransform(scrollYProgress,[0,1],["0%","100%"])
+
     return (
         <div className="parallax" 
+            ref = {ref}
             style={{ 
                 background:
                     type === "services" 
@@ -10,14 +25,16 @@ const Parallax = ({type}) => {
                     : "linear-gradient(180deg, #111132, #505064)",
                     
                 }}>
-            <h1>{type === "services"
+            <motion.h1 style={{y: yText }}>{type === "services"
                 ? "Mis servicios" 
                 : "Mis trabajos"
-            }</h1>
+            }</motion.h1>
 
-            <div className="mountains"></div>
-            <div className="planets"></div>
-            <div className="stars"></div>
+            <motion.div className="mountains"></motion.div>
+            <motion.div className="planets" style={{y: yBg,
+                backgroundImage: `url($type==="services"?"/planets.png":"/sun.png"})`
+            }}></motion.div>
+            <motion.div style={{x: yBg }} className="stars"></motion.div>
 
         </div>
     )
