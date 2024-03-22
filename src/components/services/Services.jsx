88dd9d8010b2b279e172bd8e801525e0b767d487
marc7hedev <driberstagg@gmail.com>
2024-03-22 13:17:47 -0600

    Implementadas animaciones de framerMotion en sección de Servicios

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,15 +1,32 @@
 import "./services.scss";
 import {motion} from "framer-motion";
 
+const variants = {
+    initial:{
+        x:-500,
+        y:100,
+        opacity:0,
+    },
+    animate:{
+        x:0,
+        opacity: 1,
+        y:0,
+        transition:{
+            duration:1,
+            staggerChildren:0.1,
+        },
+    },
+};
+
 
 const Services = () => {
     return (
-    <motion.div className="services">
-        <motion.div className="textContainer">
+    <motion.div className="services" variants={variants} initial="initial" animate="animate">
+        <motion.div className="textContainer" variants={variants}>
             <p>I focus on helping you brand grow<br /> and move forward</p>
             <hr />
         </motion.div>
-        <motion.div className="titleContainer">
+        <motion.div className="titleContainer" variants={variants}>
             <div className="title">
                 <img src="/people.webp" alt="" />
                 <h1>
@@ -23,7 +40,7 @@ const Services = () => {
                 <button>MIS SERVICIOS</button>
             </div>
         </motion.div>
-        <motion.div className="listContainer">
+        <motion.div className="listContainer" variants={variants}>
             <motion.div className="box" 
             whileHover={{background: "lightgray", color: "black"}}>
                 <h2>Aplicaciones web</h2>
@@ -33,21 +50,21 @@ const Services = () => {
                 <button>Solicitar</button>
             </motion.div>
             <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
-                <h2>Aplicaciones web</h2>
+                <h2>Automatizaciones</h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti saepe officia consequatur delectus ea minima, et mollitia ab, adipisci ad necessitatibus animi neque libero. Hic assumenda accusamus doloremque magnam voluptatibus?
                 </p>
                 <button>Solicitar</button>
             </motion.div>
             <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
-                <h2>Aplicaciones web</h2>
+                <h2>Chatbots IA</h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti saepe officia consequatur delectus ea minima, et mollitia ab, adipisci ad necessitatibus animi neque libero. Hic assumenda accusamus doloremque magnam voluptatibus?
                 </p>
                 <button>Solicitar</button>
             </motion.div>
             <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
-                <h2>Aplicaciones web</h2>
+                <h2>Seguridad</h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti saepe officia consequatur delectus ea minima, et mollitia ab, adipisci ad necessitatibus animi neque libero. Hic assumenda accusamus doloremque magnam voluptatibus?
                 </p>
