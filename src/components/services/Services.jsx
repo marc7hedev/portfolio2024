150237394d7f57076a91ba96532e47579cdef2c6
marc7 <driberstagg@gmail.com>
2024-06-07 22:23:26 -0600

    Corregido efecto hover en las cards de servicios

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -61,28 +61,28 @@ const Services = () => {
         </motion.div>
         <motion.div className="listContainer" variants={variants}>
             <motion.div className="box" 
-            whileHover={{background: "lightgray", color: "black"}}>
+            whileHover={{borderColor:"purple", boxShadow: "0 0 10px 7px rgba(128, 0, 128, 0.5)", transition: { duration: 0.3 }}}>
                 <h2>Aplicaciones web</h2>
                 <p>
                     Desarrollo de páginas web.<br />Tiendas en línea (e-commerce).<br/>Aplicaciones basadas en web.<br />Sistemas ERP.
                 </p>
                 <button>Solicitar</button>
             </motion.div>
-            <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
+            <motion.div className="box" whileHover={{borderColor:"purple", boxShadow: "0 0 10px 7px rgba(128, 0, 128, 0.5)", transition: { duration: 0.3 }}}>
                 <h2>Automatizaciones</h2>
                 <p>
                     Implementación de sistemas automatizados que se encarguen de realizar de manera eficiente las tareas repetitivas, mostrando los resultados en Dashboards.
                 </p>
                 <button>Solicitar</button>
             </motion.div>
-            <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
+            <motion.div className="box" whileHover={{borderColor:"purple", boxShadow: "0 0 10px 7px rgba(128, 0, 128, 0.5)", transition: { duration: 0.3 }}}>
                 <h2>Chatbots IA</h2>
                 <p>
                     Asistentes virtuales.<br />Soporte técnico.<br />Agentes de ventas.<br />Chatbots a la medida según tus necesidades.
                 </p>
                 <button>Solicitar</button>
             </motion.div>
-            <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
+            <motion.div className="box" whileHover={{borderColor:"purple", boxShadow: "0 0 10px 7px rgba(128, 0, 128, 0.5)", transition: { duration: 0.3 }}}>
                 <h2>Seguridad</h2>
                 <p>
                     Te ayudo a implementar las mejores medidas de seguridad en tus proyectos y plataformas, protegiéndote así de ciber ataques que pueden hacerle mucho daño a tu negocio.
